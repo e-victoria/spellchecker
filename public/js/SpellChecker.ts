6a72b5fea4d82a4fe00022790a6fa0a5453e8bed
e-victoria <evictoria@protonmail.com>
2020-04-20 14:53:46 +0200

    Find replacements for mispelled words

--- public/js/SpellChecker.ts
+++ public/js/SpellChecker.ts
@@ -1,8 +1,11 @@
 import InputReader from './InputReader';
 import StringHasher from './StringHasher';
 import FileParser from './FileParser';
+import { stringify } from 'querystring';
 
 export default class SpellChecker {
+    private abc = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    private wordsList: Map<string, boolean>;
     init() {
         const that = this;
         function callback(word: () => void) {
@@ -17,9 +20,34 @@ export default class SpellChecker {
         const fileReader: FileParser = new FileParser(filepath)
         const stringHasher: StringHasher = new StringHasher();
 
-        const wordsList: Map<string, boolean> = fileReader.readFile();
-        if (wordsList.has(word)) {
-            alert('match!');
+        this.wordsList = fileReader.readFile();
+        if (this.wordsList.has(word)) {
+            alert('ok, it\'s a word!');
         }
+        else {
+            const replacement = this.findWordReplacement(word);
+            if (replacement.length > 0) {
+                alert(`Maybe you meant: ${replacement}?`);
+            } else {
+                alert('No replacements :(');
+            }
+        }
+    }
+
+    findWordReplacement(word): Array<string> {
+        let replacement: Array<string> = [];
+        let newWord: string;
+
+        for (let i: number = 0; i < word.length - 1; i++) {
+            for (let letter of this.abc) {
+                newWord = word.substring(0, i) + letter + word.substring(i + 1, word.length);
+                if (this.wordsList.has(newWord)) {
+                    console.log(newWord);
+                    replacement.push(newWord);
+                }
+            }
+        }
+
+        return replacement;
     }
 }
